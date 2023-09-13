f150f8d40ec1669b21f6bb81040a9f0148b1923a
Vladimir Redkin <vsredkin01@gmail.com>
2023-09-14 02:05:39 +0300

    fix recursive globbing

--- buildscript/fileutils.py
+++ buildscript/fileutils.py
@@ -3,7 +3,7 @@ import pathlib
 
 
 def find_all_images() -> list[str]:
-    return glob.glob("*.psd", recursive=True)
+    return glob.glob("**/*.psd", recursive=True)
 
 
 def get_matching_png_name(psd_name: str) -> pathlib.Path:
